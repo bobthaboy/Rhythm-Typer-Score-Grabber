5b87bbf8b9661746f1400fa82d5e2f67afa49f8f
Bob Thaboy <bobthaboy@gmail.com>
2026-05-01 00:46:54 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from time import sleep
 import csv
 
 rt = RhythmTyperAPI()
-player = "goink"
 
 
 def map_scrape():
@@ -25,20 +24,29 @@ def map_scrape():
     return beatmaps
 
 
-def rank_grabber(beatmaps, rank):
+def rank_grabber(player, plays, beatmaps, rank):
+    exclude = False
     scores = []
     i = 0
     length = sum(len(beatmap.difficulties) for beatmap in beatmaps)
 
+    print(f"(y/n) exclude scores where you already tie #1? Warning: if not, the code will take {len(plays)}s longer.")
+    if input("") == "y".lower():
+        exclude = True
+        length -= len(plays)
+
     for beatmap in beatmaps:
         for dif in beatmap.difficulties:
+            if (exclude and beatmap.beatmap_id in [play.beatmap_id for play in plays]
+                    and dif.difficulty_name in [play.difficulty for play in plays]):
+                continue
             score = 0
             ranks = set()
             lb = rt.beatmap_leaderboard(beatmap_id=beatmap.beatmap_id, difficulty=dif.difficulty_name)
             sleep(1)
             for user in lb:
                 ranks.add(user.score)
-                if user.username == player:
+                if user.username == player.username:
                     score = user.score
             ordered = sorted(ranks, reverse=True)
             i += 1
@@ -71,19 +79,28 @@ def rank_grabber(beatmaps, rank):
             ])
 
 
-def cunny_grabber(beatmaps, target):
+def cunny_grabber(player, plays, beatmaps, target):
+    exclude = False
     scores = []
     i = 0
     length = sum(len(beatmap.difficulties) for beatmap in beatmaps)
 
+    print(f"(y/n) exclude scores where you already tie #1? Warning: if not, the code will take {len(plays)}s longer.")
+    if input("") == "y".lower():
+        exclude = True
+        length -= len(plays)
+
     for beatmap in beatmaps:
         for dif in beatmap.difficulties:
+            if (exclude and beatmap.beatmap_id in [play.beatmap_id for play in plays]
+                    and dif.difficulty_name in [play.difficulty for play in plays]):
+                continue
             score = 0
             fuck_score = 0
             lb = rt.beatmap_leaderboard(beatmap_id=beatmap.beatmap_id, difficulty=dif.difficulty_name)
             sleep(1)
             for user in lb:
-                if user.username == player:
+                if user.username == player.username:
                     score = user.score
                 if user.username == target:
                     fuck_score = user.score
@@ -112,7 +129,7 @@ def cunny_grabber(beatmaps, target):
             ])
 
 
-def fc_grabber(beatmaps):
+def fc_grabber(player, beatmaps):
     with open("output.csv", "w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
 
@@ -133,7 +150,7 @@ def fc_grabber(beatmaps):
                 notes = dif.tap_count + dif.hold_count * 2 + dif.typing_count
                 notes = int(notes)
                 for user in lb:
-                    if user.username == player:
+                    if user.username == player.username:
                         combo = user.combo
                         if combo < notes:
                             writer.writerow([
@@ -143,6 +160,22 @@ def fc_grabber(beatmaps):
 
 
 def main():
+    with open("user_id.txt", "r") as file:
+        user_id = file.readline().strip()
+
+    if not user_id:
+        print("Problem in user_id.txt. Please put your firebase uid in there.")
+        exit()
+
+    try:
+        player = rt.get_user_profile(user_id)
+        sleep(1)
+        plays = rt.first_place_scores(user_id).scores
+        sleep(1)
+    except KeyError:
+        print("Problem in user_id.txt. Please put your firebase uid in there.")
+        exit()
+
     print("Alright boss, what are we doing today? Do you want:")
     print("1. Compare my scores to a rank on all map leaderboards")
     print("2. Compare my scores to a player on all map leaderboards")
@@ -154,22 +187,25 @@ def main():
         print("Return scores from which leaderboard position? (ex: \"1\" compares you to all first place scores.) ")
         try:
             rank = int(input())
-            rank_grabber(map_scrape(), rank)
+            rank_grabber(player, plays, map_scrape(), rank)
         except ValueError:
             print(f"Hey dumbass, ever stop to think \"Huh, maybe that ain't a number?\"")
             print("Fuck you.")
             print("I'm closing the code.")
+            exit()
 
     elif command == "2":
         print("Return scores from which player? (ex: \"andrew\". Case sensitive!)")
         target = input()
-        cunny_grabber(map_scrape(), target)
+        cunny_grabber(player, plays, map_scrape(), target)
 
     elif command == "3":
-        fc_grabber(map_scrape())
+        fc_grabber(player, map_scrape())
+
     else:
         print(f"well DAMN jackie, i can't {command}!")
         exit()
 
 
 main()
+print("Done! Check \"output.csv\", you should be able to convert it into an Excel spreadsheet.")
